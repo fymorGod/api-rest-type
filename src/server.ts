a12fa21e60bcf58011631c6831ceabc3bbf1e2d5
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-03-21 15:25:54 -0300

    projeto inicio

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+
+
+
+app.listen((3000), () => {
+    console.log("server is running...")
+});
