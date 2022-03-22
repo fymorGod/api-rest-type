f40cf131b6cab2f410fdd02d792e532a55bd98c9
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-03-22 10:43:03 -0300

    alteraçõo no .gitignore

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,5 +1,13 @@
 import {createConnection} from "typeorm"
 
+//arquivo de conexão com o banco de dados
+
+/**
+ * Os dados de cadastro da conexão com o banco de dados
+ * vem do arquivo ormconfig 
+ * que está localizado na raiz do projeto
+ */
+
 createConnection().then(() => {
     console.log('Conected with database')
 })
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
+//Arquivo principal da API
 import "reflect-metadata";
 import express from "express";
 import './database/connect'
 
 const app = express();
 
+//aplicando conversão para json no express, parq que seja possível receber dados em json
 app.use(express.json())
 
 
 
-
+//estabelecendo conexão da api
 app.listen((3000), () => {
     console.log("server is running...")
 });
